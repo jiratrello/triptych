d0131fd0d8c03aae50bed58830da16e25aa8c723
troy [viorama macbook] <mail@troyduguid.com>
2017-03-20 19:25:02 +0100

    created unity project

---
+++ triptych/Assets/_scripts/ResetObject.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class ResetObject : Editor {
+
+	[MenuItem("GameObject/Reset Position To Origin %#0")] //x % is ctrl/command, # is shift
+	static void Reset () {
+		// if we have something selected
+		if (Selection.activeGameObject != null) {
+			// record an undo state on the transform before we change it so ctrl-z works
+			Undo.RecordObject (Selection.activeGameObject.transform, "Moved to origin");
+			Selection.activeGameObject.transform.position = Vector3.zero;
+		}
+	}
+}
