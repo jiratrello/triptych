223a38a2d5f5f214829f2f758c900f9faec07896
troyduguid // muumuu PC <mail@troyduguid.com>
2017-04-03 22:10:35 +0200

    - started camera switching scripts, got getting skybox settings from other game objects working

---
+++ triptych/Assets/_scripts/TestScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour {
+
+	public Material mat;
+
+	private Renderer rend;
+
+	// Use this for initialization
+	void Start () {
+
+		rend = GetComponent<Renderer> ();
+		rend.material = mat;
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ triptych/Assets/_scripts/camera/PlayerCamera.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour {
+
+	Skybox mySky;
+
+	CameraVariables camVars;
+
+	// Use this for initialization
+	void Start () {
+		
+		mySky = GetComponent<Skybox> ();
+
+	}
+
+	void Update () {
+
+		if (Input.GetMouseButton(0)) {
+			FirstFunction ();
+		}
+
+		if (Input.GetMouseButton(1)) {
+			SecondFunction ();
+		}
+
+		if (Input.GetMouseButton(2)) {
+			ThirdFunction ();
+		}
+	}
+
+	void FirstFunction() {
+		print ("this is the first function");
+		GameObject obj = GameObject.Find ("OBJ1");
+		camVars = obj.GetComponent<CameraVariables> ();
+		SetCamVars();
+	}
+
+	void SecondFunction() {
+		print ("this is the second function");
+		GameObject obj = GameObject.Find ("OBJ2");
+		camVars = obj.GetComponent<CameraVariables> ();
+		SetCamVars();
+	}
+
+	void ThirdFunction() {
+		print ("this is the third function");
+		GameObject obj = GameObject.Find ("OBJ3");
+		camVars = obj.GetComponent<CameraVariables> ();
+		SetCamVars();
+	}
+
+	void SetCamVars () {
+		mySky.material = camVars.sky;
+
+	}
+		
+}
