45cd32f5cdd10e9fa00e0bad90091e37583ceed9
troy [viorama macbook] <mail@troyduguid.com>
2017-04-06 22:47:05 +0200

    proper commit (last one did not upload new script)

---
+++ triptych/Assets/_scripts/camera/MouseLook.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour {
+
+	public float lookSensitivity = 5f;
+	public float xRotation;
+	public float yRotation;
+	public float currentXRotation;
+	public float currentYRotation;
+	public float xRotationV;
+	public float yRotationV;
+	public float lookSmoothDamp = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		xRotation -= Input.GetAxis ("Mouse Y") * lookSensitivity;
+		yRotation += Input.GetAxis ("Mouse X") * lookSensitivity;
+
+		//Clamp camera so it cannot 360 rotate upside down
+		//xRotation = Mathf.Clamp (xRotation, -90, 90);
+
+		transform.rotation = Quaternion.Euler (xRotation, yRotation, 0);
+
+
+	}
+}
