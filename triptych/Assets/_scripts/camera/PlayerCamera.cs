a19b344fc5ba0e74e4230244f4bb7dc514180594
troyduguid // muumuu PC <mail@troyduguid.com>
2017-04-11 00:37:40 +0200

    - begun three button input - begun raycast hitting only specific tag

--- triptych/Assets/_scripts/camera/PlayerCamera.cs
+++ triptych/Assets/_scripts/camera/PlayerCamera.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class PlayerCamera : MonoBehaviour {
 
+	//player controls
 	public float lookSensitivity = 5f;
 	public float xRotation;
 	public float yRotation;
+	public float zRotation;
 
+	//camera properties
+	CameraVariables camVars;
 	Camera myCam;
 	Skybox mySky;
 	float myFov;
-
-	CameraVariables camVars;
 
 	void Start () {
 
@@ -28,29 +30,34 @@ public class PlayerCamera : MonoBehaviour {
 			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 			RaycastHit hit;
 			if (Physics.Raycast(ray, out hit)) {
-				// the object identified by hit.transform was clicked
-				camVars = hit.transform.GetComponent<CameraVariables> ();
+				if (hit.transform.tag == "ClickableObject")
+					// the object identified by hit.transform was clicked
+					camVars = hit.transform.GetComponent<CameraVariables> ();
 				SetCamVars ();
 			}
 		}
 
-		if (Input.GetKeyDown("space")){ // if right button pressed...
+		if (Input.GetMouseButton(1)){ // if right button pressed...
 			print("right button presssed");
 			Cursor.visible = false;
 
-			xRotation -= Input.GetAxis ("Mouse Y") * lookSensitivity;
-			yRotation += Input.GetAxis ("Mouse X") * lookSensitivity;
+			xRotation -= Input.GetAxis("Mouse Y") * lookSensitivity;
+			yRotation += Input.GetAxis("Mouse X") * lookSensitivity;
 
-			transform.rotation = Quaternion.Euler (xRotation, yRotation, 0);
-		} else if (Input.GetKeyUp ("space")) {
+			transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+
+		} else if (Input.GetMouseButtonUp (1)) {
 			Cursor.visible = true;
+		}
+
+		if (Input.GetMouseButton(2)) {
+			zRotation -= Input.GetAxis ("Mouse ScrollWheel") * lookSensitivity;
+			transform.rotation = Quaternion.Euler (0, 0, zRotation);
 		}
 	}
 
 	void SetCamVars () {
 		mySky.material = camVars.sky;
-
 		myCam.fieldOfView = camVars.fov;
-
 	}
 }
